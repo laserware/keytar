a1ec13cea810aeb65d68f229fb678f02f6a9fa85
Mike Rourke <mike.w.rourke@gmail.com>
2024-07-29 21:02:47 -0500

    feat: add `handleChords` function with tests

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,12 @@
 export { getChordDisplay } from "./getChordDisplay.ts";
+export { handleChords } from "./handleChords.ts";
 export { isChordPressed } from "./isChordPressed.ts";
 export { isPrintableCharPressed, isPrintableChar } from "./printableChars.ts";
-export { Key, Modifier, MouseButton, type Chord } from "./types.ts";
+export {
+  Key,
+  Modifier,
+  MouseButton,
+  type Chord,
+  type ChordedEvent,
+  type Token,
+} from "./types.ts";
