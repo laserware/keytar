f0ad3224827ea0f2dd5b220db357a20fbfb0a11b
Mike Rourke <mike.w.rourke@gmail.com>
2025-07-08 16:46:24 -0500

    test: ensure macOS is used for chord display

--- test.setup.ts
+++ test.setup.ts
@@ -1,3 +1,9 @@
+import { mock } from "bun:test";
+
 import { GlobalRegistrator } from "@happy-dom/global-registrator";
+
+mock.module("@laserware/arcade", () => ({
+  isPlatform: (platform: string) => platform === "mac",
+}));
 
 GlobalRegistrator.register();
